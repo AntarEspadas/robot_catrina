c412ad53ead8afbce25cab817644ccd4a19ce0b9
Antar Espadas <antar.espadas@hotmail.com>
2023-10-24 22:25:38 +0100

    Actualiza código arduino para fucionar con pines arbitrarios

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,16 +1,12 @@
 #include <Servo.h>
 
-Servo servos[6];
+Servo servos[9];
 
 void setup() {
   Serial.begin(9600);
 
-  int servo_pins[] = {3, 5, 6, 9, 10, 11};
-
-  for (int i = 0; i < 6; i++) {
-    int pin = servo_pins[i];
-    //servos[i] = new Servo();
-    servos[i].attach(pin);
+  for (int i = 0; i < 9; i++) {
+    servos[i].attach(i + 3);
     servos[i].write(90);
   }
 }
@@ -21,30 +17,11 @@ void loop() {
 
     Serial.readBytes(values, 2);
 
-    int i = index(values[0]);
+    int i = values[0] - 3;
     
     servos[i].write(values[1]);
 
     // Ok Message
     Serial.write(1);
-  }
-}
-
-int index(int pin) {
-  switch (pin) {
-    case 3:
-      return 0;
-    case 5:
-      return 1;
-    case 6:
-      return 2;
-    case 9:
-      return 3;
-    case 10:
-      return 4;
-    case 11:
-      return 5;
-    default:
-      return -1;
   }
 }
